46f1f19292134948bafc5a8844fd262ef37933d7
Sekani <mohammad.kurdy.2012@gmail.com>
2019-01-22 00:19:09 +0300

    Add project files.

---
+++ Aras/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Aras.Startup))]
+namespace Aras
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
