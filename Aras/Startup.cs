74d64378dd1f59ea7873acf51dd54898a2298757
MiladAbdullah <milad.chey@gmail.com>
2019-01-22 23:55:36 +0300

    new comment

--- Aras/Startup.cs
+++ Aras/Startup.cs
@@ -4,6 +4,7 @@ using Owin;
 [assembly: OwinStartupAttribute(typeof(Aras.Startup))]
 namespace Aras
 {
+    // new comment
     public partial class Startup {
         public void Configuration(IAppBuilder app) {
             ConfigureAuth(app);
