d2ed382105a648f4d9ac649b37b415e996ac0ec9
dawd <26520520+dawdjawhar@users.noreply.github.com>
2019-02-03 12:00:48 -0800

    Defaulte Design

--- Aras/Site.Master.designer.cs
+++ Aras/Site.Master.designer.cs
@@ -13,12 +13,39 @@ namespace Aras {
     public partial class SiteMaster {
         
         /// <summary>
-        /// MainContent control.
+        /// form1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder MainContent;
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// usersShow control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl usersShow;
+        
+        /// <summary>
+        /// purchaseShow control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl purchaseShow;
+        
+        /// <summary>
+        /// registerUser control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl registerUser;
     }
 }
