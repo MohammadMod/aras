302225e3c6d1fc8040ab13a749da61b478886259
Sekani <mohammad.kurdy.2012@gmail.com>
2019-04-07 00:53:49 +0300

    a

---
+++ Aras/Driver Amount.aspx.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Aras
+{
+    public partial class Driver_Amount : System.Web.UI.Page
+    {
+        SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["con"].ToString());
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                DataTable subjectss = new DataTable();
+
+                SqlDataAdapter adapterr = new SqlDataAdapter("select [complite_name] from [dbo].[regester_table]", conn);
+                adapterr.Fill(subjectss);
+
+                conn.Open();
+                DropDownList1.DataSource = subjectss;
+                DropDownList1.DataTextField = "complite_name";
+                DropDownList1.DataValueField = "complite_name";
+                DropDownList1.DataBind();
+                DropDownList1.Items.Insert(0, new ListItem("Select", "NA"));
+
+                conn.Close();
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("driver_amount", conn);
+            cmd.Parameters.AddWithValue("driver", DropDownList1.SelectedItem.Text);
+            cmd.CommandType = CommandType.StoredProcedure;
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            conn.Close();
+            if (ds.Tables[0].Rows.Count > 0)
+            {
+                GridView1.DataSource = ds;
+                GridView1.DataBind();
+            }
+            else
+            {
+                ds.Tables[0].Rows.Add(ds.Tables[0].NewRow());
+                GridView1.DataSource = ds;
+                GridView1.DataBind();
+                int columncount = GridView1.Rows[0].Cells.Count;
+                GridView1.Rows[0].Cells.Clear();
+                GridView1.Rows[0].Cells.Add(new TableCell());
+                GridView1.Rows[0].Cells[0].ColumnSpan = columncount;
+                GridView1.Rows[0].Cells[0].Text = "No Records Found";
+            }
+        }
+    }
+}
